a7c633bab00b883f71bbbd3f5413212229125a77
Sife El Din <eng.sifeeldinahmed@gmail.com>
2022-04-09 00:29:34 +0200

    finished of api calls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './index.css';
-import Button from './components/Button';
-import Header from './components/Header';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
